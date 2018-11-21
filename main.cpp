cbc12114122e7cc5da4f3bf15b5a4c4478a8a011
xQsme <2151163@my.ipleiria.pt>
2018-11-21 20:25:36 +0000

    rework

--- main.cpp
+++ main.cpp
@@ -1,81 +1,79 @@
-#include <QCommandLineParser>
-#include <QCoreApplication>
-#include <QTime>
-#include <QtDebug>
+#include <QtCore>
+#include "Packet.h"
 
 void help();
-void chooseFile(QString file);
 void interval(int millisecondsToWait);
-void total(int num);
-void delay( int millisecondsToWait );
+void start(pcpp::Packet packet, int num, int ms);
+void delay(int millisecondsToWait );
+pcpp::Packet parsePacket(QFile* file);
+int send(pcpp::Packet packet);
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-        QCommandLineParser parser;
-        QCommandLineOption helpOption(QStringList() << "h" << "help",
-                    QCoreApplication::translate("main", "Usage Help"));
-        parser.addOption(helpOption);
-        QCommandLineOption targetInputOption(QStringList() << "f" << "file",
-                    QCoreApplication::translate("main", "Specify a file"));
-        parser.addOption(targetInputOption);
-        QCommandLineOption intervalOption(QStringList() << "i" << "interval" ,
-                    QCoreApplication::translate("main", "chosse a interval in ms"));
-        parser.addOption(intervalOption);
-        QCommandLineOption totalOption(QStringList() << "t" << "total",
-                    QCoreApplication::translate("main", "amount of time to send package"));
-        parser.addOption(totalOption);
+    QCommandLineParser parser;
+    QCommandLineOption helpOption(QStringList() << "h" << "help",
+                QCoreApplication::translate("main", "Usage Help"));
+    parser.addOption(helpOption);
+    QCommandLineOption targetInputOption(QStringList() << "f" << "file",
+                QCoreApplication::translate("main", "Specify a file"));
+    parser.addOption(targetInputOption);
+    QCommandLineOption intervalOption(QStringList() << "i" << "interval" ,
+                QCoreApplication::translate("main", "chosse a interval in ms"));
+    parser.addOption(intervalOption);
+    QCommandLineOption totalOption(QStringList() << "t" << "total",
+                QCoreApplication::translate("main", "amount of time to send package"));
+    parser.addOption(totalOption);
 
-        parser.process(a);
+    parser.process(a);
 
-        QStringList args = parser.optionNames();
+    QStringList args = parser.optionNames();
 
-        if(args.contains("h") || args.contains("help")){
-            help();
-            return 0;
-        }
+    int ms=1000;
+    int total=1;
+    pcpp::Packet packet;
 
-        if(args.contains("f") || args.contains("file")){
-            QString file;
-            file=parser.value(targetInputOption);
-            chooseFile(file);
+    if(args.contains("h") || args.contains("help")){
+        help();
+        return 0;
+    }
 
-        }
+    if(args.contains("f") || args.contains("file")){
+        QFile file(parser.value(targetInputOption));
+        parsePacket(&file);
+    }
+    else {
+        help();
+        return 0;
+    }
 
-        if(args.contains("i") || args.contains("interval")){
-           int ms =  parser.value(intervalOption).toInt();
-           interval(ms);
-        }
+    if(args.contains("i") || args.contains("interval")){
+       ms =  parser.value(intervalOption).toInt();
+    }
 
-        if(args.contains("t") || args.contains("total")){
-            int num =  parser.value(totalOption).toInt();
-            total(num);
-        }
+    if(args.contains("t") || args.contains("total")){
+        total =  parser.value(totalOption).toInt();
+    }
 
-
+    start(packet, total, ms);
 
     return 0;
 }
 
 void help()
 {
-    qDebug() << "Usage:\n-f   --file\t\t\tChoose a file to send" <<
-                "\n-i   --interval\t\t\tDInterval to send package in ms" <<
-                "\n-t   --total\t\t\tamount of time to send the same package" ;
+    qDebug() << "Usage:\n-f\t--file\t\tChoose a file containing the packet to send" <<
+                "\n-i\t--interval\tInterval between packets sent in ms (Default 1000)" <<
+                "\n-t\t--total\t\tTotal packets to send (Default 1)" ;
 }
 
-void chooseFile(QString file){
-    qDebug() << "file";
-}
 
-void interval(int millisecondsToWait){
-    qDebug() << "invervalo";
+void interval(int millisecondsToWait)
+{
     delay(millisecondsToWait);
-    qDebug() << "invervalo2";
-
 }
 
-void delay( int millisecondsToWait )
+void delay(int millisecondsToWait )
 {
     QTime *dieTime = new QTime(QTime::currentTime().addMSecs( millisecondsToWait ));
     while( QTime::currentTime() < *dieTime )
@@ -84,10 +82,29 @@ void delay( int millisecondsToWait )
     }
 }
 
-void total(int num){
+pcpp::Packet parsePacket(QFile* file)
+{
+    pcpp::Packet packet;
+    return packet;
+}
 
-    /*for(int i=0;i<num;i++){
-        //send package
-    }*/
-   qDebug() << "total";
+void start(pcpp::Packet packet, int num, int ms)
+{
+    qDebug() << "Sending packet(s)...";
+    for(int i=1;i<=num;i++)
+    {
+        if(send(packet)){
+            qDebug() << "Packet sent.";
+        }
+        if(i!= num)
+        {
+            interval(ms);
+        }
+    }
+    qDebug() << "Sent" << num << "packet(s).";
+}
+
+int send(pcpp::Packet packet)
+{
+    return 1;
 }
